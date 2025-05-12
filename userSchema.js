5af853bc8eb7ec4f1e4c33a10547fff5ca963e26
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-12 11:03:19 +0530

    first commit

--- userSchema.js
+++ userSchema.js
@@ -3,6 +3,21 @@ const mongoose=require("mongoose")
 const userSchema=require (mongoose.schema)
 
 
+Username:{
+    type:String,
+    required:True;
+},
+Email:{
+    type:String,
+    required:True,
+},
+
+Password:{
+    Type:String,
+    required:True,
+},
+
+
 
 
 
